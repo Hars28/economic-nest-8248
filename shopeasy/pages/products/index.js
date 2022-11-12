dfbdc5d4520bc12c0063cb3e81e0c7f4a6c1f60a
Madhukesh K T <madhukeshthakur@gmail.com>
2022-11-12 16:44:11 +0530

    Created a routes

---
+++ shopeasy/pages/products/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import LeftBox from "../../components/productspage/Leftbox";
+
+const products = () => {
+  return (
+    <>
+      <Navbar />
+      <LeftBox />
+      <Footer />
+    </>
+  );
+};
+
+export default products;
---
+++ shopeasy/pages/womens/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import MensPage from "../../components/men/MensPage";
+import Navbar from "../../components/Navbar";
+
+const mens = () => {
+  return (
+    <>
+      <Navbar />
+      <MensPage />
+      <Footer />
+    </>
+  );
+};
+
+export default mens;
