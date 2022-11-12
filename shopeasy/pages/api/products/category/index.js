23cfa3122f042b0714af5cdd821cd8a81d01ee3f
deepumandal <nomercyhunters@gmail.com>
2022-11-12 23:17:49 +0530

    added apies updated

--- shopeasy/pages/api/products/category/index.js
+++ shopeasy/pages/api/products/category/index.js
@@ -7,14 +7,54 @@
 // 
 
 import dbConnect from "../../../../lib/dbConnect"
+import authModal from "../../../../modals/auth/authmodal"
 import categoryModal from "../../../../modals/products/category"
 
 export default async function filter(req, res) {
 
-    if (req.method !== "GET") {
-        return res.send(`this ${req.method} not valid through this route`)
+    if (req.method == "DELETE") {
+        await dbConnect()
+        // authorization 
+        const { id } = req.headers
+
+        const authorization =  await authModal.find({ "_id" : id})
+
+
+
+
+        console.log(id)
+
+        // const { id } = req.query
+        // console.log(id);
+        // const deleteitem = await categoryModal.deleteOne({ "_id": id })
+
+        return res.send({
+            // deleteitem: deleteitem,
+            massage: "category me delete acchi btt nhi hai "
+        })
     }
-  
+    if (req.method === "POST") {
+        // "image": "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/17425548/2022/3/7/0adbc936-ec99-4157-ab34-9a7226e0738f1646660549902RoadsterMenBlackSneakers1.jpg",
+        // "brand": "Roadster",
+        // "name": "Men Sneakers",
+        // "discount_price": "Rs. 872",
+        // "price": "Rs. 3795",
+        // "id": 17,
+        // "type": "shoes",
+        // "ratings": 3.5
+        await dbConnect()
+        const items = req.body
+        console.log(items)
+        const additem = new categoryModal({
+            ...items
+        })
+        await additem.save()
+        console.log("hii")
+        return res.send({
+            massage: "saved",
+        })
+
+    }
     try {
 
         const params = req.query
@@ -63,18 +103,52 @@ export default async function filter(req, res) {
         }
         // sort based on price 
         else if (params.price) {
-            const price = `Rs. ${params.price}`
+            const price = +params.price
             console.log(price)
+
+            if (params.cmd === "lte") {
+                const filtered = await categoryModal.aggregate([
+                    { $match: { "price": { $lte: price } } }, {$sort : { price : 1}}, { $limit: 20 }
+                ])
+                return res.send(filtered)
+            }
+            else if (params.cmd === "lt") {
+                const filtered = await categoryModal.aggregate([
+                    { $match: { "price": { $lt: price } } }, {$sort : { price : 1}}, { $limit: 20 }
+                ])
+                return res.send(filtered)
+            }
+            else if (params.cmd === "gte") {
+                const filtered = await categoryModal.aggregate([
+                    { $match: { "price": { $gte: price } } }, {$sort : { price : 1}}, { $limit: 20 }
+                ])
+                return res.send(filtered)
+            }
+            else if ((params.cmd === "gt")) {
+                const filtered = await categoryModal.aggregate([
+                    { $match: { "price": { $gt: price } } }, {$sort : { price : 1}}, { $limit: 20 }
+                ])
+                return res.send(filtered)
+            }
+
+
+
+
+
+
+
+
             const filtered = await categoryModal.aggregate([
-                { $match: { "price": { $lte: price } } }
+                { $match: { "price": { $lte: price } } }, {$sort : { price : -1}}, { $limit: 20 }
             ])
+            console.log(filtered);
             return res.send(filtered)
         }
         else if (params.discount_price) {
-            const discount = `Rs. ${params.discount_price}`
-            console.log(discount)
+            const discount = params.discount_price
+            // console.log(discount)
             const filtered = await categoryModal.aggregate([
-                { $match: { "discount_price": { $lte: discount } } }
+                { $match: { "discount_price": { $lte: discount } } },{$sort : { price : -1}}, { $limit: 20 }
             ])
             return res.send(filtered)
         }
@@ -82,7 +156,7 @@ export default async function filter(req, res) {
 
         const filtered = await categoryModal.aggregate([{ $match: params }, {
             $sort: { totalOrderValue: -1 }
-        }])
+        }, { $limit: 20 }])
         console.log(filtered)
 
         res.send({
