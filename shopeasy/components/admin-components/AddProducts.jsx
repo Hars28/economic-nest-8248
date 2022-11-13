0672e858ca8f5fc5ad78e1f4380bca0b4e5d4c85
Govind Singh <govindkmr141@gmail.com>
2022-11-13 18:13:02 +0530

    crud added for admin

--- shopeasy/components/admin-components/AddProducts.jsx
+++ shopeasy/components/admin-components/AddProducts.jsx
@@ -11,8 +11,49 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-export default function AddProducts({ isOpen, onOpen, onClose }) {
+export default function AddProducts({
+  isOpen,
+  onOpen,
+  onClose,
+  setA,
+  setLoading,
+}) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+  const [type, setType] = useState("");
+  const session = useSession();
+
+  const addData = async () => {
+    setLoading(true);
+    const id = session.data.user.objId;
+    let obj = {
+      name,
+      image,
+      price: +price,
+      type,
+    };
+
+    axios({
+      method: "POST",
+      url: `http://localhost:3000/api/products/category`,
+      data: obj,
+      headers: {
+        id,
+      },
+    }).then((res) => {
+      setA((a) => a + 1);
+      setLoading(false);
+    });
+
+    // console.log(obj);
+    onClose();
+  };
+
   return (
     <>
       <Modal blockScrollOnMount={false} isOpen={onOpen} onClose={onClose}>
@@ -24,22 +65,40 @@ export default function AddProducts({ isOpen, onOpen, onClose }) {
             <FormControl>
               <Box mt="0.8rem">
                 <FormLabel>Product Name</FormLabel>
-                <Input type="text" placeholder="Product Name" />
+                <Input
+                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  placeholder="Product Name"
+                />
               </Box>
               <Box mt="0.8rem">
                 <FormLabel>Image Url</FormLabel>
-                <Input type="text" placeholder="image url" />
+                <Input
+                  onChange={(e) => setImage(e.target.value)}
+                  type="text"
+                  placeholder="image url"
+                />
               </Box>
               <Box mt="0.8rem">
                 <FormLabel>Category</FormLabel>
-                <Input type="text" placeholder="category" />
+                <Input
+                  onChange={(e) => setType(e.target.value)}
+                  type="text"
+                  placeholder="category"
+                />
               </Box>
               <Box mt="0.8rem">
                 <FormLabel>Price</FormLabel>
-                <Input type="number" placeholder="price" />
+                <Input
+                  onChange={(e) => setPrice(e.target.value)}
+                  type="number"
+                  placeholder="price"
+                />
               </Box>
               <Box textAlign="center" mt="1rem">
-                <Button colorScheme="green">Submit</Button>
+                <Button onClick={addData} colorScheme="green">
+                  Submit
+                </Button>
               </Box>
             </FormControl>
           </ModalBody>
